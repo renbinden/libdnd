854baba9597cb6e53de9d6c6f524cd27502d9033
Ren Binden <ren.binden@gmail.com>
2022-05-01 13:19:17 +0100

    Allow ExperienceLookupTable to be subclassed

--- src/main/java/uk/co/renbinden/libdnd/experience/ExperienceLookupTable.java
+++ src/main/java/uk/co/renbinden/libdnd/experience/ExperienceLookupTable.java
@@ -1,42 +1,53 @@
 package uk.co.renbinden.libdnd.experience;
 
-public final class ExperienceLookupTable {
+public abstract class ExperienceLookupTable {
 
-    private ExperienceLookupTable() {}
+    public abstract int getExperienceRequiredForLevel(int level);
+    public abstract int getLevelAtExperience(int experience);
+    public abstract int getMaxExperience();
 
-    public static final int MAX_EXPERIENCE = 355000;
+    public static final ExperienceLookupTable DEFAULT = new ExperienceLookupTable() {
 
-    public static int getExperienceRequiredForLevel(int level) {
-        return switch (level) {
-            case 1 -> 0;
-            case 2 -> 300;
-            case 3 -> 900;
-            case 4 -> 2700;
-            case 5 -> 6500;
-            case 6 -> 14000;
-            case 7 -> 23000;
-            case 8 -> 34000;
-            case 9 -> 48000;
-            case 10 -> 64000;
-            case 11 -> 85000;
-            case 12 -> 100000;
-            case 13 -> 120000;
-            case 14 -> 140000;
-            case 15 -> 165000;
-            case 16 -> 195000;
-            case 17 -> 225000;
-            case 18 -> 265000;
-            case 19 -> 305000;
-            case 20 -> 355000;
-            default -> Integer.MAX_VALUE;
-        };
-    }
+        private static final int MAX_EXPERIENCE = 355000;
 
-    public static int getLevelAtExperience(int experience) {
-        int level = 1;
-        while (getExperienceRequiredForLevel(level + 1) <= experience) {
-            level++;
+        public int getExperienceRequiredForLevel(int level) {
+            return switch (level) {
+                case 1 -> 0;
+                case 2 -> 300;
+                case 3 -> 900;
+                case 4 -> 2700;
+                case 5 -> 6500;
+                case 6 -> 14000;
+                case 7 -> 23000;
+                case 8 -> 34000;
+                case 9 -> 48000;
+                case 10 -> 64000;
+                case 11 -> 85000;
+                case 12 -> 100000;
+                case 13 -> 120000;
+                case 14 -> 140000;
+                case 15 -> 165000;
+                case 16 -> 195000;
+                case 17 -> 225000;
+                case 18 -> 265000;
+                case 19 -> 305000;
+                case 20 -> 355000;
+                default -> Integer.MAX_VALUE;
+            };
         }
-        return level;
-    }
+
+        public int getLevelAtExperience(int experience) {
+            int level = 1;
+            while (getExperienceRequiredForLevel(level + 1) <= experience) {
+                level++;
+            }
+            return level;
+        }
+
+        @Override
+        public int getMaxExperience() {
+            return MAX_EXPERIENCE;
+        }
+
+    };
 }
